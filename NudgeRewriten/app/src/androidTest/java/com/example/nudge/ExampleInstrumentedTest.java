787e9bdfb2f648fe83937dbb66e32f42e6d76624
initishh <nitishkumarchoudhary3@gmail.com>
2019-06-10 19:19:50 +0530

    Fixes done

--- NudgeRewriten/app/src/androidTest/java/com/example/nudge/ExampleInstrumentedTest.java
+++ NudgeRewriten/app/src/androidTest/java/com/example/nudge/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.nudgerewriten", appContext.getPackageName());
+        assertEquals("com.example.nudge", appContext.getPackageName());
     }
 }
